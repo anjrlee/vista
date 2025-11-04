6825c844774fa9f29b59d39414f2268f1e177e44
anjrlee <102264715+anjrlee@users.noreply.github.com>
2025-11-04 14:23:41 +0800

    deploy things

--- backend/routes/test.py
+++ backend/routes/test.py
@@ -16,27 +16,3 @@ def test():
 
 
 
-load_dotenv()
-cred_path = os.getenv("FIREBASE_CREDENTIAL_PATH")
-if not cred_path:
-    raise ValueError("FIREBASE_CREDENTIAL_PATH 沒有設定")
-
-cred = credentials.Certificate(cred_path)
-firebase_admin.initialize_app(cred)
-
-
-db = firestore.client()
-
-@router.post("/testFirebase")
-def test():
-    now = datetime.now().isoformat()
-    doc_ref = db.collection("test_collection").document()  # 自動產生id的document
-
-    data = {
-        "timestamp": now
-    }
-
-    doc_ref.set(data)  # 寫入 Firestore
-
-    return {"message": "寫入成功", "datetime": now}
-
